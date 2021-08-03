405051fd7eb60a16386b6820532e4acde05489f6
suja venkatesan <suja.venkatesan@Syncfusion.com>
2021-08-03 10:51:03 +0530

    Adding sample for sorting

---
+++ SortingDemo/SortingDemo/SortingDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace SortingDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MDAxQDMxMzgyZTMxMmUzMEovYURla2tjaGVWblNLV21ZWnM0MWZ5cjBXYXpRa05oeE5wZHpJUGVDTms9");
+            MainPage = new DataGridPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SortingDemo/SortingDemo/SortingDemo/View/DataGridPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.Data;
+using Syncfusion.SfDataGrid.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SortingDemo
+{
+    public partial class DataGridPage : ContentPage
+    {
+        ViewModel viewModel;
+        public DataGridPage()
+        {
+            InitializeComponent();
+            viewModel = new ViewModel();
+            dataGrid.ItemsSource = viewModel.OrdersInfo;
+            dataGrid.AllowSorting = true;
+            dataGrid.GridLoaded += DataGrid_GridLoaded;
+            dataGrid.SortColumnDescriptions.Add(new SortColumnDescription() { ColumnName = "EmployeeID",SortDirection=ListSortDirection.Descending });
+        }
+
+        private void DataGrid_GridLoaded(object sender, GridLoadedEventArgs e)
+        {
+            dataGrid.LiveDataUpdateMode = LiveDataUpdateMode.AllowDataShaping;
+        }
+
+        private void AddItem_Clicked(object sender, EventArgs e)
+        {
+            viewModel.OrdersInfo.Add(new OrderInfo(58, 1796, "Linod", "Michael", "Mendoza"));
+            viewModel.OrdersInfo.Add(new OrderInfo(50, 1794, "Furib", "Danielle", "Keefe"));
+            viewModel.OrdersInfo.Add(new OrderInfo(51, 1794, "Folko", "William", "Owens"));
+            viewModel.OrdersInfo.Add(new OrderInfo(56, 1787, "Blonp", "Brenda", "Betts"));
+        }
+    }
+}
